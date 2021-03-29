9663e510a20f47d90995065e4ef48b8bb5db332c
cedouiri <cedouiri@gmail.com>
2021-03-29 23:47:03 +0100

    update

--- 0x05-personal_data/filtered_logger.py
+++ 0x05-personal_data/filtered_logger.py
@@ -10,6 +10,9 @@ from typing import List
 
 
 def filter_datum(fields: List, redaction: str, msg: str, sep: str) -> str:
-    '''Returns a log msg.'''
-    return (sep.join(x if x.split('=')[0] not in fields
+    '''
+    Returns a log msg
+
+    '''
+    return (';'.join(x if x.split('=')[0] not in fields
             else re.sub(r'=.*', '=' + redaction, x) for x in msg.split(sep)))
