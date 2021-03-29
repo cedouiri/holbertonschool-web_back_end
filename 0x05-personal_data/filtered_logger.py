b3f9f3fe4a703597b97290e601d4ae0e5d0d3bc7
cedouiri <cedouiri@gmail.com>
2021-03-29 23:39:48 +0100

    Regex-ing

---
+++ 0x05-personal_data/filtered_logger.py
@@ -0,0 +1,15 @@
+#!/usr/bin/env python3
+'''
+Regex-ing
+
+'''
+
+
+import re
+from typing import List
+
+
+def filter_datum(fields: List, redaction: str, msg: str, sep: str) -> str:
+    '''Returns a log msg.'''
+    return (sep.join(x if x.split('=')[0] not in fields
+            else re.sub(r'=.*', '=' + redaction, x) for x in msg.split(sep)))
